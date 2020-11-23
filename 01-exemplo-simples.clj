5d34e97cb805cae953cd94ba66a51e5b8068affc
Gabriel Coutinho <gcoutinhorj@gmail.com>
2020-11-22 22:54:14 -0300

    adicionando comentários

--- 01-exemplo-simples.clj
+++ 01-exemplo-simples.clj
@@ -1,11 +1,11 @@
 (defmacro unless
   "Parecido com if mas nega a condição"
-  [condicao & codigo]
-  `(if (not ~condicao)
-    ~@codigo))
+  [condicao & codigo]   ; Recebe como parâmetro a condição
+  `(if (not ~condicao)  ; Se a negação da condição for verdadeira
+    ~@codigo))          ; Executa o código recebido
 
 ;; E então pode ser usada da seguinte forma
 (let [x 10]
   (unless (= x 10)
     (println "x diferente de 10")
-    (println "x é igual a 10"))) 
+    (println "x é igual a 10")))
